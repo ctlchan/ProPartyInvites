6313dd678486b0bc97b1bad2e97397d4179b11b4
Chris <christakleungchan@gmail.com>
2024-06-10 22:09:46 -0400

    Implement validation summary

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,9 +19,22 @@ public class HomeController : Controller
     [HttpPost]
     public ViewResult RsvpForm(GuestResponse guestResponse)
     {
-        // Uses the static Repository class to store the filled-object
-        Repository.AddResponse(guestResponse);
-        // We can't invoke View() because we don't want to send it back to an empty form
-        return View("Thanks", guestResponse);
+        // ModelState is built into base class Controller - details the outcome of the model binding process
+        if (ModelState.IsValid) 
+        {
+            // Uses the static Repository class to store the filled-object
+            Repository.AddResponse(guestResponse);
+            // We can't invoke View() because we don't want to send it back to an empty form
+            return View("Thanks", guestResponse);
+        } 
+        // Razor has access to validation details - loading the default view will display validation errors as necessary
+        else
+            return View();
+    }
+    [HttpGet]
+    public ViewResult ListResponses()
+    {
+        return View(Repository.Responses
+                .Where(r => r.WillAttend == true));
     }
 }
